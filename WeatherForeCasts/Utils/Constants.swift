8d1695330208fc4a2d103b74ac782a860662a6e4
LinhMAC <nguyenlinh@LinhMACs-MacBook-Pro.local>
2023-11-29 02:30:57 +0700

    update 29/11

---
+++ WeatherForeCasts/Utils/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  WeatherForeCasts
+//
+//  Created by LinhMAC on 28/11/2023.
+//
+
+import Foundation
---
+++ WeatherForeCasts/Utils/StandardForm.swift
@@ -0,0 +1,8 @@
+//
+//  StandardForm.swift
+//  WeatherForeCasts
+//
+//  Created by LinhMAC on 25/11/2023.
+//
+
+import Foundation
