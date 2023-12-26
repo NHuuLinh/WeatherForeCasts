1b05c72499d6f8d0c4283858243ea09ad36f563f
LinhMAC <nguyenlinh@LinhMACs-MacBook-Pro.local>
2023-12-26 22:26:46 +0700

    qa

---
+++ WeatherForeCasts/HomeScreen/Maps/HistoryCell/LocationHistoryTitleTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LocationHistoryTitleTableViewCell.swift
+//  WeatherForeCasts
+//
+//  Created by LinhMAC on 26/12/2023.
+//
+
+import UIKit
+
+class LocationHistoryTitleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ WeatherForeCasts/HomeScreen/Maps/HistoryCell/SelectedLocationsHistoryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SelectedLocationsHistoryTableViewCell.swift
+//  WeatherForeCasts
+//
+//  Created by LinhMAC on 25/12/2023.
+//
+
+import UIKit
+
+class SelectedLocationsHistoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
