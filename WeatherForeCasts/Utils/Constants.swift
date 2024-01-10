83e17ccff5dc3a195ab6fad89cf8cc16cc2987bb
LinhMAC <nguyenlinh@LinhMACs-MacBook-Pro.local>
2024-01-10 13:58:54 +0700

    update weatherapi key

--- WeatherForeCasts/Utils/Constants.swift
+++ WeatherForeCasts/Utils/Constants.swift
@@ -10,12 +10,13 @@ import UIKit
 import Alamofire
 class Constants {
     static let baseUrl = "http://api.weatherapi.com/v1/forecast.json"
-    static let key = "29c1ac8be3e14dc4ade164900232512"
+    static let key = "034b0e0c77a749c4ac965653241001"
     static let errorTextFieldBackgroundColor = UIColor(red: 1, green: 0.95, blue: 0.97, alpha: 1)
     static let defaultTextFieldBackgroundColor = UIColor.white
     static let defaultBtnBackgroundColor = UIColor.white
     static let disableBtnBackgroundColor = UIColor.darkGray
     static var themeColor = UIColor.white
 }
+
 
 
